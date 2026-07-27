8631836e6f2b824606fe13bf2e88bfc3d37b1d04
Varshanath <varshanathkutz@gmail.com>
2026-07-27 12:13:06 +0530

    Fix frontend folder tracked as submodule

---
+++ frontend/src/environments/environment.ts
@@ -0,0 +1,12 @@
+export const environment = {
+  production: false,
+  apiUrl: 'https://testsheets.vsnaptechnology.com/api',
+  authUrl: 'https://testmail.vsnaptechnology.com/api/auth',
+  wsUrl: 'wss://testsheets.vsnaptechnology.com',
+};
+
+// export const environment = {
+//   production: false,
+//   apiUrl: 'http://192.168.0.187:8000/api',
+//   wsUrl: 'ws://192.168.0.187:8000',
+// };
